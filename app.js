47cd42286545b534260849d682e269492b3be3e1
Zoran Janjic <janjiczoran23@gmail.com>
2021-05-14 12:45:27 +0200

    Add .gitignore, procfile and update Heroku conection

--- app.js
+++ app.js
@@ -124,7 +124,13 @@ app.post("/compose", function(req, res) {
 });
 // END OF COMPOSE
 
-// SERVER
-app.listen(3000, function() {
-  console.log("Server running on: 3000");
+// If no heroku port that use local
+let port = process.env.PORT;
+if (port = null || port = "") {
+  port = 3000;
+}
+}
+
+app.listen(port, function() {
+  console.log("Server running on: " + port);
 });
